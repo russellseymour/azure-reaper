e586eb7b1bd737346231a8423c528f4e5dc33b29
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-07-05 13:00:18 +0100

    Add support to shutdown AKS clusters #7 Added new PowerSchedule class Added tests
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- function/Lib/Reaper.cs
+++ function/Lib/Reaper.cs
@@ -5,6 +5,7 @@ using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.Management.Compute.Fluent;
 using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ContainerService.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 using Microsoft.Extensions.Logging;
 using System;
@@ -15,6 +16,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Azure.Reaper.Lib
 {
@@ -24,15 +26,21 @@ namespace Azure.Reaper.Lib
         private ILog _logger;
         private NotificationDelay _notificationDelay;
         private IBackend _backend;
+        private IAzure azure;
+        private List<Models.Setting> settings;
+        private IEnumerable<LocationTZ> timezones;
+        private DateTime timeNowUtc;
+        private Models.PowerStatistics powerStats;
 
-        public Reaper(IBackend backend, ILog logger, NotificationDelay notificationDelay)
+        public Reaper(IBackend backend, ILog logger, List<Models.Setting> settings, NotificationDelay notificationDelay)
         {
             _backend = backend;
             _logger = logger;
             _notificationDelay = notificationDelay;
+            this.settings = settings;
         }
 
-        public bool Process(List<Models.Setting> settings, List<Models.Subscription> subscriptions, Models.LocationTZ locationTZ)
+        public bool Process(List<Models.Subscription> subscriptions, Models.LocationTZ locationTZ)
         {
 
             // bool error = false;
@@ -48,7 +56,7 @@ namespace Azure.Reaper.Lib
             bool manage_vms = Convert.ToBoolean(settings.First(s => s.name == "manage_vms").value);
 
             // get a list of the timezones to work with
-            IEnumerable<LocationTZ> timezones = locationTZ.GetByName().GetData();
+            timezones = locationTZ.GetByName().GetData();
 
             // Create a connection to Slack
             // ensure that the webhook url can be turned into a URI
@@ -86,7 +94,7 @@ namespace Azure.Reaper.Lib
                 // IEnumerable<LocationTZ> timezones = timezone.GetAll();
 
                 // Define timestamp to work against so it is the same time for all checks
-                DateTime timeNowUtc = DateTime.UtcNow;
+                // DateTime timeNowUtc = DateTime.UtcNow;
 
                 // Create a NotificationDelay object to use to
                 // get previous notification alerts and add new ones
@@ -97,27 +105,20 @@ namespace Azure.Reaper.Lib
                 {
 
                     // only proceed if the reaper has been enabled on the subscription
+                    /*
                     if (!sub.reaper) {
                         _logger.Information("Reaper is not enabled on subscription: {name} [{id}]", sub.name, sub.subscription_id);
                         continue;
                     }
+                    */
 
                     // Determine the timenow to be used as criteria
                     // This is done on each subscription because there might be a lot of groups to enumerate
-                    DateTime _time_now = DateTime.UtcNow;
-                    string time_now = _time_now.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                    timeNowUtc = DateTime.UtcNow;
+                    string time_now = timeNowUtc.ToString("yyyy-MM-ddTHH:mm:ssZ");
 
                     // Login to Azure with the credentials for this subscription
-                    IAzure azure = Utilities.AzureLogin(sub, AzureEnvironment.AzureGlobalCloud, _logger);
-
-                    // AzureCredentials credentials = new AzureCredentials(
-                    //     new ServicePrincipalLoginInformation
-                    //     {
-                    //         ClientId = sub.client_id,
-                    //         ClientSecret = sub.client_secret
-                    //     }, sub.tenant_id, AzureEnvironment.AzureGlobalCloud
-                    // );
-                    // Microsoft.Azure.Management.Fluent.Azure azure = Microsoft.Azure.Management.Fluent.Azure.Configure().Authenticate(credentials).WithSubscription(sub.subscription_id);
+                    azure = Utilities.AzureLogin(sub, AzureEnvironment.AzureGlobalCloud, _logger);
 
                     // get a list of resource groups in the subscription
                     IEnumerable<IResourceGroup> resource_groups = azure.ResourceGroups.List();
@@ -184,11 +185,15 @@ namespace Azure.Reaper.Lib
                         else
                         {
 
+                            // Initialise the powerstatistics to be used for notifications
+                            powerStats = new PowerStatistics();
+
                             // So that the Slack user is not inundated with messages, ensure that all machines that have been
                             // found to be in violation of running times are bundled up
-                            List<string> stopped = new List<string>();
-                            List<string> started = new List<string>();
+                            // List<string> stopped = new List<string>();
+                            // List<string> started = new List<string>();
 
+/*
                             // as the resource group is valid look at the virtual machines that are running
                             // and determine if they should be shutdown or not
                             IEnumerable<IVirtualMachine> vms = azure.VirtualMachines.ListByResourceGroup(resource_group.Name);
@@ -230,9 +235,14 @@ namespace Azure.Reaper.Lib
                                 nd.name = virtualMachine.GetName();
 
                                 await _notificationDelay.Upsert(nd);
-                                */
+                                
                                 
                             }
+                            */
+
+                            manageVirtualMachines(resource_group);
+
+                            manageK8sClusters(resource_group);
 
                             // only send out messages for VMs if there are some
                             //if (vms.Count() > 0 && (started.Count > 0 || stopped.Count > 0))
@@ -259,8 +269,150 @@ namespace Azure.Reaper.Lib
                     }
                 }
             // }
-
+ 
             return true;
+        }
+
+        /// <summary>
+        /// Find the Kubernetes clusters in the specified resource group and determine if they need to be
+        /// left alone, shutdown or startedup
+        /// </summary>
+        /// <param name="rgName">Name of hte resource group to look for clusters in</param>
+        private void manageK8sClusters(IResourceGroup rg) {
+
+            // determine if any kubernetes clusters exist in the group
+            IEnumerable<IKubernetesCluster> clusters = azure.KubernetesClusters.ListByResourceGroup(rg.Name);
+
+            if (clusters.Count() == 0) {
+                _logger.Information("No AKS clusters in group: {0}", rg.Name);
+            } else {
+
+                _logger.Information("Analysing AKS Clusters: {0}", clusters.Count().ToString());
+
+                // iterate around the clusters
+                foreach (IKubernetesCluster cluster in clusters) {
+
+                    string operation = String.Empty;
+                    string powerState = cluster.Inner.PowerState.Code.Value;
+
+                    // Determine if the cluster should be running or not
+                    PowerSchedule powerSchedule = new PowerSchedule(
+                        settings, 
+                        timezones, 
+                        _logger,
+                        timeNowUtc,
+                        rg.Tags, 
+                        cluster.Tags, 
+                        powerState, 
+                        cluster.RegionName
+                    );
+
+                    powerSchedule.Process();
+
+                    // use the powerschedule information to determine if the resource should be running
+                    if (powerSchedule.StartResource())
+                    {
+                        powerStats.StartResource("aks", cluster.Name);
+                        operation = "start";
+                    }
+
+                    if (powerSchedule.StopResource())
+                    {
+                        powerStats.StopResource("aks", cluster.Name);
+                        operation = "stop";
+                    }
+
+                    if (!String.IsNullOrEmpty(operation))
+                    {
+                        // build up the URL
+                        object[] replacements = { cluster.Manager.RestClient.BaseUri, cluster.Manager.SubscriptionId, rg.Name, cluster.Name, operation };
+                        string url = String.Format("{0}subscriptions/{1}/resourceGroups/{2}/providers/Microsoft.ContainerService/managedClusters/{3}/{4}?api-version=2021-05-01", replacements);
+
+                        _logger.Information(url);
+
+                        // Create a new HttpClient top 
+                        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+                        CancellationToken cancellationToken = new CancellationToken();
+                        HttpClient httpClient = new HttpClient();
+
+                        cluster.Manager.RestClient.Credentials.ProcessHttpRequestAsync(request, cancellationToken).GetAwaiter().GetResult();
+                        HttpResponseMessage response = httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken).GetAwaiter().GetResult();
+                        
+                        _logger.Information(response.Content.ReadAsStringAsync().Result);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Find the virtual machines in the group and determine what ones need to have
+        /// the powerstate modified
+        /// </summary>
+        /// <param name="rg"></param>
+        private void manageVirtualMachines(IResourceGroup rg) {
+
+            // get a list of the virtual machines in the group
+            IEnumerable<IVirtualMachine> virtualMachines = azure.VirtualMachines.ListByResourceGroup(rg.Name);
+
+            // check the beginning of the name of hte resource group
+            // if it starts with MC_ then it will be a managed cluster and Reaper should not deal
+            // with the virtual machines directly
+            if (rg.Name.ToLower().StartsWith("mc_"))
+            {
+                _logger.Information("Not managing Virtual Machines in managed AKS cluster: {0}", rg.Name);
+                return;
+            }
+
+            if (virtualMachines.Count() == 0)
+            {
+                _logger.Information("No Virtual Machines in group: {0}", rg.Name);
+                return;
+            }
+
+            _logger.Information("Analysing Virtual Machines: {0}", virtualMachines.Count().ToString());
+
+            // Determine if VMs are to be managed, return if they are not
+            bool manage_vms = Convert.ToBoolean(settings.First(s => s.name == "manage_vms").value);
+            if (!manage_vms) {
+                _logger.Information("Reaper is not set to manage virtual machines, please check your settings");
+                return;
+            }
+
+            foreach (IVirtualMachine vm in virtualMachines)
+            {
+                // get the powerstate of the vm
+                string powerstate = vm.PowerState.ToString();
+
+                // Create a powerschedule object to determine if the machine should be running or not
+                PowerSchedule powerSchedule = new PowerSchedule(
+                    settings,
+                    timezones,
+                    _logger,
+                    timeNowUtc,
+                    rg.Tags,
+                    vm.Tags,
+                    powerstate,
+                    vm.RegionName
+                );
+
+                powerSchedule.Process();
+
+                // start or stop the resource based on the results from the powerschedule
+                if (powerSchedule.StartResource())
+                {
+                    powerStats.StartResource("vm", vm.Name);
+                    _logger.Information("[{0}] Starting virtual machine: {1}", rg.Name, vm.Name);
+                    vm.StartAsync();
+                }
+
+                if (powerSchedule.StopResource())
+                {
+                    powerStats.StopResource("vm", vm.Name);
+                    _logger.Information("[{0}] Stopping virtual machine: {1}", rg.Name, vm.Name);
+                    vm.DeallocateAsync();
+                }
+
+            }
         }
     }
 }
---
+++ tests/PowerScheduleTests.cs
@@ -0,0 +1,317 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using Azure.Reaper.Lib;
+
+namespace Tests
+{
+
+    public class PowerScheduleTests : IClassFixture<TestsFixture>
+    {
+        private const string POWER_STATE = "powerstate/running";
+        private const string REGION = "ukwest";
+        private const string TIMEZONE = "GMT Standard Time";
+        TestsFixture data;
+
+        public PowerScheduleTests(TestsFixture data)
+        {
+            this.data = data;
+        }
+
+        [Fact]
+        public void MergeTagsNoConflicts()
+        {
+
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            groupTags.Add("ownerEmail", "me@example.com");
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                DateTime.UtcNow,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            // Get the merged tags
+            Dictionary<string, string> tags = powerSchedule.Tags;
+
+            // assert that there is one item and it is the ownerEmail
+            Assert.Equal(1, tags.Count);
+            Assert.Contains("ownerEmail", tags.Keys);
+            Assert.Equal("me@example.com", tags["ownerEmail"]);
+        }
+
+        [Fact]
+        public void MergeTagsWithConflicts()
+        {
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            groupTags.Add("ownerEmail", "me@example.com");
+            resourceTags.Add("ownerEmail", "example@me.com");
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                DateTime.UtcNow,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            // Get the merged tags
+            Dictionary<string, string> tags = powerSchedule.Tags;
+
+            // assert that there is one item and it is the ownerEmail
+            Assert.Equal(1, tags.Count);
+            Assert.Contains("ownerEmail", tags.Keys);
+            Assert.Equal("example@me.com", tags["ownerEmail"]);            
+        }
+
+        [Fact]
+        public void HasAndGetTag()
+        {
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            groupTags.Add("STARTSTOPTIME", "09:00 - 2000");
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                DateTime.UtcNow,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            Assert.True(powerSchedule.HasTag("tag_vm_start_stop_time"));
+            Assert.Equal("09:00 - 2000", powerSchedule.GetTag("tag_vm_start_stop_time"));
+        }
+
+        [Fact]
+        public void GetSchedule()
+        {
+
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            groupTags.Add("STARTSTOPTIME", "09:00 - 2000");
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                DateTime.UtcNow,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            // get the schedule from the tags
+            powerSchedule.ScheduleFromTags();
+
+            // create the timespan to measure against
+            TimeSpan startTime = new TimeSpan(9, 0, 0);
+            TimeSpan stopTime = new TimeSpan(20, 0, 0);
+
+            // assert that the start and stop times are correct
+            Assert.Equal(startTime, powerSchedule.tsStart);
+            Assert.Equal(stopTime, powerSchedule.tsStop);
+        }
+
+        [Fact]
+        /// <summary>
+        /// This will get set the timezone based on the region of the resource
+        /// </summary>
+        public void GetTimeZoneFromRegion() {
+            
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                DateTime.UtcNow,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            powerSchedule.SetTimeZone();
+
+            // ensure that the zoneInfo is correctly set
+            TimeZoneInfo zoneInfo = TimeZoneInfo.FindSystemTimeZoneById(TIMEZONE);
+            Assert.Equal(zoneInfo, powerSchedule.zoneInfo);
+        }
+
+        [Fact]
+        /// <summary>
+        /// Test that the timezone can be set by applying a Tag to the resource
+        /// </summary>
+        public void GetTimeZoneFromTag()
+        {
+            string TEST_TIMEZONE_ID = "Mountain Standard Time";
+
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            // Add the tag in to state the timezone that should be used
+            resourceTags.Add("TIMEZONE", TEST_TIMEZONE_ID);
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                DateTime.UtcNow,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            powerSchedule.SetTimeZone();
+
+            // ensure that the zoneInfo is correctly set
+            TimeZoneInfo zoneInfo = TimeZoneInfo.FindSystemTimeZoneById(TEST_TIMEZONE_ID);
+            Assert.Equal(zoneInfo, powerSchedule.zoneInfo);
+        }
+
+        [Fact]
+        public void ResourcePermittedToRunGMT() 
+        {
+            // set the time and the timezone to work with
+            DateTime timeReference = new DateTime(2021, 07, 02); 
+
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                timeReference,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            // ensure that the resource should be running now
+            powerSchedule.SetTimeZone();
+            powerSchedule.PermittedToRunDay();
+
+            Assert.True(powerSchedule.PermittedToRunOnDay);
+        }
+
+        [Fact]
+        public void ResourceNotPermittedToRunGMT() 
+        {
+            // set the time and the timezone to work with
+            DateTime timeReference = new DateTime(2021, 07, 03); 
+
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                timeReference,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            // ensure that the resource should be running Now
+            powerSchedule.SetTimeZone();
+            powerSchedule.PermittedToRunDay();
+
+            Assert.False(powerSchedule.PermittedToRunOnDay);
+        }        
+
+        [Fact]
+        public void ResourcePermittedToRunMountainTime()
+        {
+            string TEST_TIMEZONE_ID = "Mountain Standard Time";
+
+            // set the time and the timezone to work with
+            DateTime timeReference = new DateTime(2021, 07, 03, 0, 1, 0); 
+
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            // Add the tag in to state the timezone that should be used
+            resourceTags.Add("TIMEZONE", TEST_TIMEZONE_ID);
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                timeReference,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            // ensure that the resource should be running now
+            powerSchedule.SetTimeZone();
+            powerSchedule.PermittedToRunDay();
+
+            Assert.True(powerSchedule.PermittedToRunOnDay);            
+        }
+
+        [Fact]
+        public void ResourceShouldBeStopped()
+        {
+            // set the time and the timezone to work with
+            DateTime timeReference = new DateTime(2021, 07, 02, 21, 0, 0); 
+
+            // create the tags for the resource group and the 
+            Dictionary<string, string> groupTags = new Dictionary<string, string>();
+            Dictionary<string, string> resourceTags = new Dictionary<string, string>();
+
+            PowerSchedule powerSchedule = new PowerSchedule(
+                data.settings,
+                data.timeZones,
+                data.logger,
+                timeReference,
+                groupTags,
+                resourceTags,
+                POWER_STATE,
+                REGION
+            );
+
+            // ensure that the resource should be running now
+            powerSchedule.SetTimeZone();
+            powerSchedule.PermittedToRunDay();
+            powerSchedule.ShouldBeRunning();
+
+            Assert.True(powerSchedule.StopResource());
+        }
+    }
+}
